995de87a0d16a291df651bbc3700e9323ddc9cca
Victor Hugo Braguim Canto <vbraguimcanto@gmail.com>
2018-06-26 16:00:38 -0300

    Create metodoBisseccao.py

---
+++ metodoBisseccao.py
@@ -0,0 +1,22 @@
+import sys, math
+def f(x):
+	return x**2 + math.log(x)
+def bisseccao(erro,xa,xb):
+	xk = (xa+xb)/float(2)
+	while abs(xb - xa)/2 > erro:
+		print f(xk)
+		if f(xk) == 0:
+			return xk
+		elif f(xa)*f(xk) < 0:
+			xb = xk
+		else:
+			xa = xk
+		xk = (xa+xb)/float(2)
+	return xk
+def main():
+	a = float(input("Digite o valor de xa: "))
+	b = float(input("Digite o valor de xb: "))
+	erro = float(input("Digite o valor do erro: "))
+	print 'Raiz Aproximada:',
+	print bisseccao(erro,a,b)
+main()
